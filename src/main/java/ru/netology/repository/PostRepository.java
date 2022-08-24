e3570e511d09972ab2dfd83f42f9a21b22ecf709
Евгений Маслеников <ev.maslenikov@mail.ru>
2022-08-24 11:27:25 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,55 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Vector;
+
+public class PostRepository {
+  private final List<Post> allPosts;
+  private long idCounter;
+
+  public PostRepository() {
+    this.allPosts = new Vector<>();
+    idCounter = 0;
+  }
+
+  public List<Post> all() {
+    return allPosts;
+  }
+
+  public Optional<Post> getById(long id) {
+
+    for (Post post : allPosts) {
+      if (id == post.getId()) {
+        return Optional.of(post);
+      }
+    }
+    return Optional.empty();
+  }
+
+  public Post save(Post savePost) {
+    long idSavePost = savePost.getId();
+
+    if (idSavePost == 0) {
+      savePost.setId(++idCounter);
+      allPosts.add(savePost);
+      return savePost;
+    } else {
+      Optional<Post> newPost = getById(idSavePost);
+      if (newPost.isPresent()) {
+        Post post = newPost.get();
+        post.setContent(savePost.getContent());
+        return post;
+      } else {
+        throw new NotFoundException(String.format("Пост с ID = %d невозможно сохранить!", idSavePost));
+      }
+    }
+  }
+
+  public void removeById(long id) {
+    allPosts.removeIf(post -> id == post.getId());
+  }
+}
